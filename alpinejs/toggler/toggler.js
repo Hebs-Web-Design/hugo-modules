7dbf18f5aaf987cf36cf85e945542d13ad1580af
Andrew Heberle <andrew.heberle@gmail.com>
2023-04-04 11:42:30 +0800

    Add delayclose to togger

--- alpinejs/toggler/toggler.js
+++ alpinejs/toggler/toggler.js
@@ -1,19 +1,44 @@
+// initial state can be set
 export default (open = false) => ({
     open: open,
+    closecount: 0,
 
+    // are we visible?
     get visible() {
         return this.open;
     },
+
+    // show thing
     show() {
         this.open = true;
+        this.closecount = 0;
     },
+
+    // hide and close do the same thing
     hide() {
         this.close();
     },
     close() {
         this.open = false;
     },
+
+    // delay close until after "n" tries
+    delayclose(event, n = 1) {
+        if (this.closecount >= n) {
+            this.close();
+
+            return;
+        }
+
+        this.closecount++;
+    },
+
+    // toggle state
     toggle() {
-        this.open = ! this.open;
+        if (this.open) {
+            this.close();
+        } else {
+            this.show();
+        }
     }
 });
