4e4b795663d07ff6253f6a558d12a753977beeb2
Andrew Heberle <andrewheberle@users.noreply.github.com>
2023-05-26 06:34:32 +0000

    Update main.js

--- teams-directory/assets/js/main.js
+++ teams-directory/assets/js/main.js
@@ -4,6 +4,7 @@ import intersect from '@alpinejs/intersect';
 import focus from '@alpinejs/focus';
 import list from 'js/directory/list';
 import item from 'js/directory/item';
+import * as directoryconfig from '@directoryconfig';
 
 Alpine.data('list', list);
 Alpine.data('item', item);
@@ -15,32 +16,5 @@ Alpine.plugin(intersect);
 Alpine.plugin(focus);
 
 // global stuff
-{{- with .Site.Params.directory }}
-window.directoryconfig = {
-    clientid: '{{ .ApplicationId }}',
-    tenantid: '{{ .TenantId }}', 
-    showlocation: {{ default "false" .ShowLocation }},
-    {{- with .Group }}
-    group: '{{ . }}',
-    {{- end }}
-    {{- with .SkipUsers }}
-    skipusers: {{ . | jsonify }},
-    {{- end }}
-    {{- with .Locations }}
-    locations: {{ . | jsonify }},
-    {{- end }}
-    {{- with .DefaultLocation }}
-    defaultlocation: {{ . | jsonify }},
-    {{- end }}
-    {{ if hugo.IsProduction }} useworker: {{ default "false" .UseWorker }},{{ end }}
-    {{- with .Maps }}
-    {{- with .Service }}
-    mapservice: '{{ . }}',
-    {{- end }}
-    {{- with .ApiKey }}
-    mapsapikey: '{{ . }}',
-    {{- end }}
-    {{- end }}
-};
-{{- end }}
+window.directoryconfig = directoryconfig;
 Alpine.start();
