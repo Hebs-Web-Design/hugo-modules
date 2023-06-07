2f6d526a4ccfec9af6b3a28b5c6df48e277fea1b
Andrew Heberle <andrew.heberle@r-group.com.au>
2023-06-07 11:24:33 +0800

    Change css stuff

--- teams-directory/assets/js/main.js
+++ teams-directory/assets/js/main.js
@@ -4,17 +4,18 @@ import intersect from '@alpinejs/intersect';
 import focus from '@alpinejs/focus';
 import list from 'js/directory/list';
 import item from 'js/directory/item';
-import * as directoryconfig from '@directoryconfig';
+import * as params from '@params';
 
 Alpine.data('list', list);
 Alpine.data('item', item);
 
 window.Alpine = Alpine;
 
+// load plugins
 Alpine.plugin(persist);
 Alpine.plugin(intersect);
 Alpine.plugin(focus);
 
 // global stuff
-window.directoryconfig = directoryconfig;
+window.directoryconfig = params.config;
 Alpine.start();
