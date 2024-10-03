bf2ec2f440ef4749e0773e589ea4f08dae4df1ea
Andrew Heberle <andrew.heberle@gmail.com>
2024-10-04 07:29:19 +0800

    Refactor decapcms

--- decapcms/js/decapcms.js
+++ decapcms/js/decapcms.js
@@ -3,8 +3,4 @@ window.CMS_MANUAL_INIT = true
 import { init } from 'decap-cms-app'
 import * as params from '@params'
 
-if (params.config === undefined) {
-    init()
-} else {
-    init(params.config)
-}
+init({config: params})
